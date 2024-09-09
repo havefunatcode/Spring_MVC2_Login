9ce569a1ac09a385c83aed6772799ca1b5b228df
taeho <dlxogh439@gmail.com>
2024-09-09 22:20:01 +0900

    LoginCheckFilter 구현

--- src/main/java/hello/login/config/WebConfig.java
+++ src/main/java/hello/login/config/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login.config;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckfilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,15 @@ public class WebConfig {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LoginCheckfilter());
+        filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
