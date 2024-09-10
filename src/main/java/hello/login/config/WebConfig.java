52bb5c7bbe8b26cb3aa8111462ad1e2ee00b9387
taeho <dlxogh439@gmail.com>
2024-09-10 21:51:48 +0900

    Log, Login Check Interceptor로 작성

--- src/main/java/hello/login/config/WebConfig.java
+++ src/main/java/hello/login/config/WebConfig.java
@@ -2,14 +2,30 @@ package hello.login.config;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckfilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -18,7 +34,6 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LoginCheckfilter());
